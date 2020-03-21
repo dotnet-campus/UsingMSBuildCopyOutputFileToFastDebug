0961cde3e4f078e553d3825650f44b1242fb9ebc
lindexi <lindexi_gd@163.com>
2020-03-21 09:35:43 +0800

    从启动配置

---
+++ Code/UsingMSBuildCopyOutputFileToFastDebug/LaunchSettingsParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Xml;
+using Microsoft.Build.Framework;
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Linq;
+
+namespace UsingMSBuildCopyOutputFileToFastDebug
+{
+    public class LaunchSettingsParser : Microsoft.Build.Utilities.Task
+    {
+        /// <inheritdoc />
+        public override bool Execute()
+        {
+            Debugger.Launch();
+
+            var file = Path.Combine("Properties", "launchSettings.json");
+            Console.WriteLine("开始从 launchSettings 文件读取输出文件夹");
+            Console.WriteLine($"开始读取{file}文件");
+            if (!File.Exists(file))
+            {
+                Console.WriteLine($"找不到{file}文件，读取结束");
+            }
+
+            var text = File.ReadAllText(file);
+
+
+            //var jObject = JObject.Parse(File.ReadAllText(file));
+
+            return true;
+        }
+
+        [Output]
+        public string LaunchMainProjectPath { set; get; }
+    }
+}
